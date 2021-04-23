5fd3ed38d2b1dc5a4f7f05108c8a0e8102aba466
Andrey <bakurskii2001@gmail.com>
2021-04-24 00:34:07 +0300

    add commands for selecting spheres and displaying spheres

--- database.py
+++ database.py
@@ -12,5 +12,18 @@ class Database:
     def create_moderator(self, ):
         pass
 
+    def get_spheres(self):
+        """ выдает список сфер обязательно всегда в одном и том же порядке (сортируйте по id)"""
+        # return ["Дизайн", "Создатель сайтов", "Аналитик", "Дата сайнтист"]
+        pass
+
+    def get_user_spheres(self, username):
+        """"""
+        # return ["Дизайн", "Создатель сайтов"]
+        pass
+
+    def add_spheres2user(self, username, list_id_spheres):
+        pass
+
 
 bot_db = Database()
--- server.py
+++ server.py
@@ -54,7 +54,8 @@ def choose_role(update: Update, _: CallbackContext):
 
     role = update.message.text
 
-    update.message.reply_text("Мы будем использовать Ваши имя и фамилию, которые Вы определили в профиле Telegram.",
+    update.message.reply_text("Регистрация завершена! Мы будем использовать Ваши имя и фамилию, которые Вы определили "
+                              "в профиле Telegram.",
                               reply_markup=ReplyKeyboardRemove())
 
     update.message.reply_text(text[role])
@@ -72,23 +73,90 @@ def choose_role(update: Update, _: CallbackContext):
     return ConversationHandler.END
 
 
-def cancel(update: Update, _: CallbackContext):
-    pass
+def cancel_start(update: Update, _: CallbackContext):
+    return ConversationHandler.END
+
+
+# ======= choose spheres command ==========
+CHOOSE_SPHERES_END = 1
+
+
+def choose_spheres(update: Update, _: CallbackContext):
+    """
+    выводим список всех сфер, просим выбрать и ввести номера сфер через пробел
+    """
+    update.message.reply_text(f"Выберите сферы деятельности, в которых вы работаете. Введите номера сфер через пробел.")
+
+    available_spheres(update, _)
+
+    return CHOOSE_SPHERES_END
+
+
+def choose_spheres_end(update: Update, _: CallbackContext):
+    list_id_spheres = list(map(int, update.message.text.split()))
+
+    bot_db.add_spheres2user(update.effective_user.username, list_id_spheres)
+
+    my_spheres(update, _)
+
+    return ConversationHandler.END
+
+
+def cancel_choose_spheres(update: Update, _: CallbackContext):
+    return ConversationHandler.END
+
+
+# ======= show my spheres command ========
+def my_spheres(update: Update, _: CallbackContext):
+    spheres = bot_db.get_user_spheres(update.effective_user.username)
+    list_spheres = ";\n".join([f"{ind}) {str(sphere)}" for ind, sphere in enumerate(spheres)])
+
+    update.message.reply_text(f"Ваши сферы деятельности:\n"
+                              f"{list_spheres}")
+
+
+# ======= show available spheres command =========
+def available_spheres(update: Update, _: CallbackContext):
+    spheres = bot_db.get_spheres()
+    list_spheres = ";\n".join([f"{ind}) {str(sphere)}" for ind, sphere in enumerate(spheres)])
+
+    update.message.reply_text(f"Существующие сферы деятельности:\n"
+                              f"{list_spheres}")
 
 
 def main():
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
+    # start command handler
     start_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
 
         states={
             CHOOSE_ROLE: [MessageHandler(Filters.text & ~Filters.command, choose_role)]},
 
-        fallbacks=[CommandHandler("cancel", cancel)]
+        fallbacks=[CommandHandler("cancel_start", cancel_start)]
     )
     dispatcher.add_handler(start_handler)
+
+    # choose spheres command handler
+    choose_spheres_handler = ConversationHandler(
+        entry_points=[CommandHandler('choose_spheres', choose_spheres)],
+
+        states={
+            CHOOSE_SPHERES_END: [MessageHandler(Filters.text & ~Filters.command, choose_spheres_end)]},
+
+        fallbacks=[CommandHandler("cancel_choose_spheres", cancel_choose_spheres)]
+    )
+    dispatcher.add_handler(choose_spheres_handler)
+
+    # show available spheres command handler
+    available_spheres_handler = CommandHandler('available_spheres', available_spheres)
+    dispatcher.add_handler(available_spheres_handler)
+
+    # show my spheres command handler
+    my_spheres_handler = CommandHandler('my_spheres', my_spheres)
+    dispatcher.add_handler(my_spheres_handler)
 
     updater.start_polling()
 
