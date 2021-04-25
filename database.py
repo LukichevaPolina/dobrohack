9e4682301aa6aeb8f90246a34fb9e76ccf20ab87
Lukicheva Polina <63358667+LukichevaPolina@users.noreply.github.com>
2021-04-25 17:21:54 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,20 +1,27 @@
 # Для Полины и Алены
+import sqlite3
+import datetime
+
+db = sqlite3.coonect(r'')
+cur = db.cursor()
+
 class Database:
     def __init__(self):
         self.users = []
 
-    def create_it_person(self, ):
-        pass
+    def create_it_person(self, id, surname, name):
+        cur.execute(""" INSERT INTO “ПРЕДСТАВИТЕЛЬ IT” VALUES (id, surname, name, 0);""")
 
     def create_nko_person(self, ):
-        pass
+        cur.execute(""" INSERT INTO “ПРЕДСТАВИТЕЛЬ НКО” VALUES (id, surname, name);""")
 
     def create_moderator(self, ):
-        pass
+        cur.execute("""INSERT INTO “МОДЕРАТОР” VALUES (id, surname, name);""")
 
     def get_spheres(self):
         """ выдает список сфер обязательно всегда в одном и том же порядке (сортируйте по id)"""
         # return ["Дизайн", "Создатель сайтов", "Аналитик", "Дата сайнтист"]
+        cur.execute("""SELECT "НАЗВАНИЕ" FROM """)
         pass
 
     def get_user_spheres(self, username):
