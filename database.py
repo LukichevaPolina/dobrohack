3d8a6655caefaa561e8110ca61e3816397fa0348
Lukicheva Polina <63358667+LukichevaPolina@users.noreply.github.com>
2021-04-25 19:09:51 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,36 +1,34 @@
 # Для Полины и Алены
-import sqlite3
-import datetime
-
-db = sqlite3.coonect(r'')
-cur = db.cursor()
+import psycopg2
 
 class Database:
     def __init__(self):
-        self.users = []
+        db = psycopg2.connect("database.db")
+        self.cur = db.cursor()
+        # self.users = []
 
     def create_it_person(self, id, surname, name):
-        cur.execute(""" INSERT INTO “ПРЕДСТАВИТЕЛЬ IT” VALUES (id, surname, name, 0);""")
+        self.cur.execute(""" INSERT INTO “ПРЕДСТАВИТЕЛЬ IT” VALUES (id, surname, name, 0); """)
 
     def create_nko_person(self, ):
-        cur.execute(""" INSERT INTO “ПРЕДСТАВИТЕЛЬ НКО” VALUES (id, surname, name);""")
+        self.cur.execute(""" INSERT INTO “ПРЕДСТАВИТЕЛЬ НКО” VALUES (id, surname, name); """)
 
     def create_moderator(self, ):
-        cur.execute("""INSERT INTO “МОДЕРАТОР” VALUES (id, surname, name);""")
+        self.cur.execute(""" INSERT INTO “МОДЕРАТОР” VALUES (id, surname, name); """)
 
     def get_spheres(self):
         """ выдает список сфер обязательно всегда в одном и том же порядке (сортируйте по id)"""
         # return ["Дизайн", "Создатель сайтов", "Аналитик", "Дата сайнтист"]
-        cur.execute("""SELECT "НАЗВАНИЕ" FROM """)
-        pass
+        self.cur.execute(""" SELECT "НАЗВАНИЕ" FROM "СФЕРА"; """)
 
     def get_user_spheres(self, username):
         """"""
-        # return ["Дизайн", "Создатель сайтов"]
-        pass
+        self.cur.execute(""" SELECT * FROM "СФЕРА - IT" WHERE ID=username; """)
 
     def add_spheres2user(self, username, list_id_spheres):
-        pass
+        for id_sphere in list_id_spheres:
+            self.cur.execute(""" INSERT INTO “СФЕРА - IT” VALUES (id_sphere, username); """)
+
 
 
 bot_db = Database()
